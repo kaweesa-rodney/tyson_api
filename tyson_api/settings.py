6224dc88f2a51ca52b0c41ba19b937a9fd4f1b96
Rodney Kaweesa <kaweesarodney@gmail.com>
2022-02-21 18:29:04 +0300

    heroku deployment

--- tyson_api/settings.py
+++ tyson_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,5 @@ JWT_SECRET_KEY = open(r".\\salt.key", "rb").read()    #os.environ.get('JWT_SECRE
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
