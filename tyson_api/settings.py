2e15a9c9050efc939b46cbe72de8f81347cacd43
rodkaw <kaweesarodney@gmail.com>
2022-02-23 22:08:48 +0300

    allowed hosts added

--- tyson_api/settings.py
+++ tyson_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$4%8adle&fpxt_s=k1&1kaagc0r*ivoxy16p3n*)-!bc)@(9nh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['46.101.19.128']
 
 
 # Application definition
