8fa464f36eb76b7bba4d076c03eabf2bbdf4bfcd
Rodney Kaweesa <kaweesarodney@gmail.com>
2022-02-27 16:04:13 +0300

    authentication set back

--- tyson_api/settings.py
+++ tyson_api/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':[],
-    'DEFAULT_PERMISSION_CLASSES':[],
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'authentication.backends.JWTAuthentication',
+    )
 }
 
 
